3dd1249aed2461911bfca2fdabc198e31132c3bd
Shawn GB <god.shawn@gmail.com>
2025-10-29 11:37:47 +0100

    chore(setup): add local api dev setup and initial types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello World");
+//types
+
+interface Book {
+  title: string;
+  subtitle: string;
+  isbn: string;
+  abstract: string;
+  numPages: number;
+  author: string;
+  publisher: string;
+  price: string;
+  cover: string;
+}
+
+type Books = Book[];
+
+type UpdateBook = Partial<Book>;
+
+interface ValidationErrorDetail {
+  msg: string;
+  param: string;
+  location: string;
+}
+
+interface ResponseError {
+  errors: Record<string, ValidationErrorDetail>;
+}
