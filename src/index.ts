caee2c36c15d495f9bee45f3041514fdc7f818ae
Shawn GB <god.shawn@gmail.com>
2025-10-29 16:29:50 +0100

    feat(search): implement book search functionality
    - Add search input with debouncing to filter books
    
    - Create `searchBooks` function in the API module to fetch filtered books
    
    - Create `getBook` function to fetch a single book by ISBN
    
    - Refactor initial data loading and rendering into an `init` function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { getBooks } from "./modules/api.js";
-
-console.log("JS mounted");
-
 /* TODO
  * 1. get data (books)
  * 2. safe data (let or local storage?)
@@ -10,45 +6,78 @@ console.log("JS mounted");
  * 5. fetch call for detail of filter from local
  */
 
-// should be moved to .env for prod
+import { getBooks, searchBooks } from "./modules/api.js";
+
+const searchInput = document.getElementById("search") as HTMLInputElement;
 const tbody = document.querySelector("tbody") as HTMLTableSectionElement;
+const DEBOUNCE_INTERVAL = 300;
+let debounceTimer: number | undefined;
+let books: Book[] = [];
 
-let books: Book[] = await getBooks();
+const renderTable = (): void => {
+  if (!tbody) return;
 
-console.log(books);
-
-tbody.innerHTML = books
-  .map(
-    (book: Book): string => `<tr>
+  tbody.innerHTML = books
+    .map(
+      (book: Book): string => `<tr>
 <td>
-  <button class="button button-clear fav-btn">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="fav"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-      />
-    </svg>
-  </button>
+    <button class="button button-clear fav-btn">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="fav"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            />
+        </svg>
+    </button>
 </td>
 <td>${book.title}</td>
 <td>${book.isbn}</td>
 <td>${book.author}</td>
 <td>${book.publisher}</td>
 <td>
-  <button class="button" onclick="location.href='detail.html?isbn=${book.isbn}'">
-    Detail
-  </button>
+    <button class="button" onclick="location.href='detail.html?isbn=${book.isbn}'">
+        Detail
+    </button>
 </td>
 </tr>`,
-  )
-  .join("");
+    )
+    .join("");
+};
 
-export {};
+//implement search function via query param book api
+
+if (searchInput) {
+  searchInput.addEventListener("input", (event) => {
+    const query = (event.target as HTMLInputElement).value;
+
+    debounceTimer && clearTimeout(debounceTimer);
+
+    debounceTimer = setTimeout(async (): Promise<void> => {
+      if (query.trim() === "") {
+        books = await getBooks();
+      } else {
+        books = await searchBooks(query);
+      }
+
+      renderTable();
+    }, DEBOUNCE_INTERVAL);
+  });
+}
+const init = async (): Promise<void> => {
+  try {
+    books = await getBooks();
+    renderTable();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+init();
--- src/modules/api.ts
+++ src/modules/api.ts
@@ -15,4 +15,33 @@ const getBooks = async (): Promise<Book[]> => {
   }
 };
 
-export { getBooks };
+const getBook = async (isbn: string): Promise<Book | undefined> => {
+  try {
+    const fetchBook = await fetch(API_BASE_URL + `/books/${isbn}`);
+
+    if (fetchBook.ok) {
+      return await fetchBook.json();
+    } else {
+      return undefined;
+    }
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+};
+
+const searchBooks = async (searchQuery: string): Promise<Book[]> => {
+  try {
+    const fetchBooks = await fetch(API_BASE_URL + `/books?q=${searchQuery}`);
+
+    if (fetchBooks.ok) {
+      return await fetchBooks.json();
+    } else {
+      return [];
+    }
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+export { getBooks, getBook, searchBooks };
